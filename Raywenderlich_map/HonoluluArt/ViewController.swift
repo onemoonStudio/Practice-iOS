944a480d2cf610d0cfa21da62bdc06d6f7a1e46f
onemoon <onemoonstudio@gmail.com>
2019-04-28 16:44:18 +0900

    edit : for ios9

--- Raywenderlich_map/HonoluluArt/ViewController.swift
+++ Raywenderlich_map/HonoluluArt/ViewController.swift
@@ -29,11 +29,109 @@
  */
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+  // MARK: - Properties
+  var artworks: [Artwork] = []
+  @IBOutlet weak var mapView: MKMapView!
+  let regionRadius: CLLocationDistance = 1000
+
+  // MARK: - View life cycle
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    // set initial location in Honolulu
+    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    centerMapOnLocation(location: initialLocation)
+
+    mapView.delegate = self
+//    mapView.register(ArtworkMarkerView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+//    mapView.register(ArtworkView.self, forAnnotationViewWithReuseIdentifier: <#T##String#>)
+//    mapView.register(ArtworkView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+    loadInitialData()
+    // 이렇게 한번에 등록할 수도 있다.
+    mapView.addAnnotations(artworks)
+
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    checkLocationAuthorizationStatus()
+  }
+
+  // MARK: - CLLocationManager
+
+  let locationManager = CLLocationManager()
+  func checkLocationAuthorizationStatus() {
+    if CLLocationManager.authorizationStatus() == .authorizedAlways {
+      mapView.showsUserLocation = true
+    } else {
+      locationManager.requestAlwaysAuthorization()
+    }
+  }
+
+  // MARK: - Helper methods
+
+  func centerMapOnLocation(location: CLLocation) {
+    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+      regionRadius, regionRadius)
+    mapView.setRegion(coordinateRegion, animated: true)
+  }
+
+  func loadInitialData() {
+    // 1
+    guard let fileName = Bundle.main.path(forResource: "PublicArt", ofType: "json")
+      else { return }
+    let optionalData = try? Data(contentsOf: URL(fileURLWithPath: fileName))
+
+    guard
+      let data = optionalData,
+      // 2
+      let json = try? JSONSerialization.jsonObject(with: data),
+      // 3
+      let dictionary = json as? [String: Any],
+      // 4
+      let works = dictionary["data"] as? [[Any]]
+      else { return }
+    // 5
+    let validWorks = works.flatMap { Artwork(json: $0) }
+    artworks.append(contentsOf: validWorks)
+  }
+
+}
+
+// MARK: - MKMapViewDelegate
+
+extension ViewController: MKMapViewDelegate {
+
+//   1
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard let annotation = annotation as? Artwork else { return nil }
+    // 2
+    let identifier = "marker"
+    var view: ArtworkView
+    if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+      as? ArtworkView { // 3
+      dequeuedView.annotation = annotation
+      view = dequeuedView
+    } else {
+      // 4
+      view = ArtworkView(annotation: annotation, reuseIdentifier: identifier)
+      view.canShowCallout = true
+      view.calloutOffset = CGPoint(x: -5, y: 5)
+      view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+    }
+    return view
+  }
+
+  func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
+               calloutAccessoryControlTapped control: UIControl) {
+    let location = view.annotation as! Artwork
+    let launchOptions = [MKLaunchOptionsDirectionsModeKey:
+      MKLaunchOptionsDirectionsModeDriving]
+    location.mapItem().openInMaps(launchOptions: launchOptions)
   }
 
 }
